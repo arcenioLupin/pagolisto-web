0b0ba0f60613a6d7b8f04f8ab8c0aaef8a639010
arcenioVilca <arcenio.vilca@acidlabs.com>
2025-08-06 18:48:22 -0500

    ultimo commit mvp

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7helggt75mg"
+    "revision": "0.udrbgfrkue8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
